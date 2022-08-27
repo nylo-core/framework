7c583b07cbc794aa0d1dd457545ce6f5480b4434
Anthony <ants52@aol.com>
2022-08-27 15:30:31 +1000

    v3.3.0 updates

--- lib/theme/helper/ny_theme.dart
+++ lib/theme/helper/ny_theme.dart
@@ -4,8 +4,8 @@ import 'package:theme_provider/theme_provider.dart';
 /// Class to help manage current theme in the app.
 class NyTheme {
   /// Changes the current theme to the new [theme]
-  /// standard light [themeName] (id is default_light_theme)
-  /// standard dark [themeName] (id is default_dark_theme)
+  /// standard light [themeName] (id is "light_theme")
+  /// standard dark [themeName] (id is "dark_theme")
   static set(BuildContext context, {required String id}) {
     ThemeProvider.controllerOf(context).setTheme(id);
   }
