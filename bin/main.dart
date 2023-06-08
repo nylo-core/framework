bd3b0db3e3948567e6b91d29fb5e607befb28551
Anthony <ants52@aol.com>
2023-06-08 13:54:57 +0700

    update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,7 @@ import 'package:nylo_framework/metro/metro.dart' as metro_cli;
 import 'package:nylo_support/metro/metro_service.dart';
 
 void main(List<String> arguments) async {
-  await MetroService.runCommand(arguments, allCommands: metro_cli.allCommands, menu: metroMenu);
+  await MetroService.runCommand(arguments,
+      allCommands: metro_cli.allCommands, menu: metroMenu);
   exit(0);
 }
