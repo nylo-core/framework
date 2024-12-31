c5172eab4abb402d960518021ca3a4c3cd80730c
Anthony <ants52@aol.com>
2024-12-31 12:28:27 +0700

    v6.5.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## [6.5.2] - 2024-12-31
+
+* Update copyright year 
+* Update pubspec.yaml
+
 ## [6.5.1] - 2024-12-29
 
 * Update pubspec.yaml
--- lib/nylo_framework.dart
+++ lib/nylo_framework.dart
@@ -68,4 +68,4 @@ export 'package:date_field/date_field.dart';
 export 'package:dio/dio.dart';
 
 /// Nylo version
-const String nyloVersion = 'v6.5.1';
+const String nyloVersion = 'v6.5.2';
